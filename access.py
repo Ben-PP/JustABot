f39a4f0256e465fff654900e7bad430a9b570e1a
Ben-PP <karel.parkkola@gmail.com>
2022-01-21 10:25:28 +0200

    Extracted authorize() to its own module.

--- access.py
+++ access.py
@@ -1,24 +1,18 @@
 import sqlite3
-
 import help
 
 async def access(message):
     message_splitted = message.content.split(" ")
     if len(message_splitted) <= 1:
         await help.help_access(message)
-        return
     elif message_splitted[1] == "help":
         await help.help_access(message)
-        return
     elif message_splitted[1] == "add":
         await add(message, message_splitted)
-        return
     elif message_splitted[1] == "remove":
         await remove(message, message_splitted)
-        return
     elif message_splitted[1] == "list":
         await list_roles(message)
-        return
     else:
         await message.channel.send("Unknown command '**"+message_splitted[1]+"**'.\nType '**!roles help**' for more commands.")
 
@@ -133,64 +127,3 @@ async def list_roles(message):
     await message.channel.send(content)
     db.commit()
     db.close()
-    
-#Returns True if user is authorized to access the command, else returns False
-def authorize(message, required_level):
-    if message.author.id == message.guild.owner.id:
-        print("Is owner")
-        return True
-    
-    has_access = False
-
-    dbname = "databases/"+str(message.guild.id)+".db"
-    db = sqlite3.connect(dbname)
-    cursor = db.cursor()
-
-    user_roles = message.author.roles
-    
-    #Checks if the role has trusted or higher access level
-    if required_level == "trusted":
-        cursor.execute("SELECT role_id FROM access_level WHERE is_trusted='True'")
-        trusted_roles = cursor.fetchall()
-        if trusted_roles != None:
-            if __is_trusted(user_roles, trusted_roles):
-                has_access = True
-            else:
-                cursor.execute("SELECT role_id FROM access_level WHERE is_admin='True'")
-                admin_roles = cursor.fetchall()
-                has_access = __is_admin(user_roles, admin_roles)
-    #Checks if the role has admin access level
-    elif required_level == "admin":
-        cursor.execute("SELECT role_id FROM access_level WHERE is_admin='True'")
-        admin_roles = cursor.fetchall()
-        if admin_roles != None:
-            has_access =  __is_admin(user_roles, admin_roles)
-    elif required_level != "owner":
-        print("Error: Incorrect access level! Choose either 'owner', 'admin' or 'trusted'")
-        db.commit()
-        db.close()
-        return False
-
-    db.commit()
-    db.close()
-    if has_access == False:
-        print("Unauthorized user.")
-    else:
-        print("Authorized user.")
-    return has_access
-
-#Checks if the role has trusted access level
-def __is_trusted(user_roles, trusted_roles):
-    for trusted_role_id in trusted_roles:
-        for user_role in user_roles:
-            if trusted_role_id[0] == user_role.id:
-                return True
-    return False
-
-#Checks if the role has admin access level
-def __is_admin(user_roles, admin_roles):
-    for admin_role in admin_roles:
-        for user_role in user_roles:
-            if user_role.id == admin_role[0]:
-                return True
-    return False
